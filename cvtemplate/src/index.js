7b810d33e14323b01358459e27e84c275d42944f
Frontend JavaScript <frontendjavascript@iMac-17.local>
2024-03-14 20:48:52 +0200

    curs2

--- cvtemplate/src/index.js
+++ cvtemplate/src/index.js
@@ -4,4 +4,9 @@ import './scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 
 import particles from 'particles.js'
+import '../node_modules/typed.js/dist/typed.umd.js'
+
 import './js/particles.js'
+
+
+import "./js/main.js"
